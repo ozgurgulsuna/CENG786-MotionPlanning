14713c8ea903e8fe75de7dc1ed37e70ea1b0b02b
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-10-26 10:21:10 +0300

    [HW1] Bug 0 sort of emerged here

--- Homeworks/HW1/resource/bug_planner.m
+++ Homeworks/HW1/resource/bug_planner.m
@@ -12,36 +12,45 @@ global sensor_range infinity;
 % samples = norm( qgoal - qstart ) / (sensor_range / 20);
 % range = linspace(0,1,samples);
 status = 1;
-range = 1;
+range = 0.1;
 x(1) = qstart(1);
 y(1) = qstart(2);
 for i=2:200
-    dist= [10 0];
-    % dist(0) = 10; % minimum distance
-    min = -pi/2; % angle of minimum distance
+    dist= [10 5];
+    min = 0; % angle of minimum distance
     % sweep the sensor around
-    for theta=0:2*pi/40:2*pi  % 10 degree steps
+    for theta=0:2*pi/50:2*pi  % 10 degree steps
         dist(2) = read_sensor(theta, [x(i-1) y(i-1)]);
         if (dist(2)<dist(1))
             dist(1) = dist(2) ;
             min = theta;
             status = 1;
         end
-        if (dist(2)<0.10) || (dist(1)<0.10)
+        if (dist(1)<0.10)
             status = 2;
         end
+        if (dist(1)> 0.4)
+            status = 3;
+        end
+
     end
-    
+
     % move in the direction orthogonal to the minimum distance
     % tangent move
     if status == 1
-        x(i) = x(i-1) + range*0.1*(cos(min+pi/2));
-        y(i) = y(i-1) + range*0.1*(sin(min+pi/2));
+        x(i) = x(i-1) + range*1*(cos(min+pi/2));
+        y(i) = y(i-1) + range*1*(sin(min+pi/2));
     end
 
     % get away
-    if status ==2
-        x(i) = x(i-1) - range*0.1*(cos(min));
-        y(i) = y(i-1) - range*0.1*(sin(min));
+    if status == 2
+        x(i) = x(i-1) - range*0.5*(cos(min));
+        y(i) = y(i-1) - range*0.5*(sin(min));
     end
+    % get in
+    if status == 3
+        x(i) = x(i-1) + range*0.5*(qgoal(1)-x(i-1));
+        y(i) = y(i-1) + range*0.5*(qgoal(2)-y(i-1));
+    end
+    status;
 end
