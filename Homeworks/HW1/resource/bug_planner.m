4be8b33e23b34314f71a906285ff5924419a890c
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-10-26 11:32:43 +0300

    [HW1] boundary following P controller, circumnav direction

--- Homeworks/HW1/resource/bug_planner.m
+++ Homeworks/HW1/resource/bug_planner.m
@@ -11,11 +11,13 @@ global sensor_range infinity;
 
 % samples = norm( qgoal - qstart ) / (sensor_range / 20);
 % range = linspace(0,1,samples);
+ref_dist = 0.25;
+circumnav_dir = -1;   % CCW = 1, CW = -1
 status = 1;
-range = 0.1;
+range = 0.075;
 x(1) = qstart(1);
 y(1) = qstart(2);
-for i=2:200
+for i=2:400
     dist= [10 5];
     min = 0; % angle of minimum distance
     % sweep the sensor around
@@ -26,7 +28,7 @@ for i=2:200
             min = theta;
             status = 1;
         end
-        if (dist(1)<0.10)
+        if (dist(1)<0.15)
             status = 2;
         end
         if (dist(1)> 0.4)
@@ -38,19 +40,19 @@ for i=2:200
     % move in the direction orthogonal to the minimum distance
     % tangent move
     if status == 1
-        x(i) = x(i-1) + range*1*(cos(min+pi/2));
-        y(i) = y(i-1) + range*1*(sin(min+pi/2));
+        x(i) = x(i-1) + range*1*(cos(min-(pi/2*circumnav_dir))) + range*1*(cos(min))*(dist(1)-ref_dist);
+        y(i) = y(i-1) + range*1*(sin(min-(pi/2*circumnav_dir))) + range*1*(sin(min))*(dist(1)-ref_dist);
     end
 
     % get away
     if status == 2
-        x(i) = x(i-1) - range*0.5*(cos(min));
-        y(i) = y(i-1) - range*0.5*(sin(min));
+        x(i) = x(i-1) - range*1*(cos(min));
+        y(i) = y(i-1) - range*1*(sin(min));
     end
     % get in
     if status == 3
-        x(i) = x(i-1) + range*0.5*(qgoal(1)-x(i-1));
-        y(i) = y(i-1) + range*0.5*(qgoal(2)-y(i-1));
+        x(i) = x(i-1) + range*1*(qgoal(1)-x(i-1));
+        y(i) = y(i-1) + range*1*(qgoal(2)-y(i-1));
     end
     status;
 end
