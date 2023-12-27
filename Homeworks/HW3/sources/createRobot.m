ea4a6f04d72e3e38a80289af18702643c94c28f8
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-12-28 02:35:17 +0300

    [HW3] last step is to sample for the second leg and combination

--- Homeworks/HW3/sources/createRobot.m
+++ Homeworks/HW3/sources/createRobot.m
@@ -18,7 +18,7 @@ function robot = createRobot(configuration,draw)
 %   Ozgur Gulsuna, METU
 %   CENG786 Robot Motion Planning and Control, Fall 2023
 
-lengths = [2 9 ; 18 2]; % Length of each arm
+lengths = [2 20 ; 2 13]; % Length of each arm
 
 % check if the configuration is valid
 if length(configuration) ~= 2 + size(lengths,1)
@@ -30,7 +30,8 @@ robot = struct('base', [configuration(1); configuration(2)], ...
                'angles', configuration(3:end), ...
                'lengths', lengths, ...
                'bodies', [], ...
-               'closed', false);
+               'closed', false, ...
+               'maxheight', 0);
 
 % make sure the angles are between 0 and 2*pi
 % for i = 1:length(lengths)
@@ -46,6 +47,7 @@ for i = 1:size(lengths,1)
     corner_3 = [corner_2(1) - robot.lengths(i,1)*sin(robot.angles(i)+previous_angle), corner_2(2) + robot.lengths(i,1)*cos(robot.angles(i)+previous_angle)];
     corner_4 = [corner_3(1) - robot.lengths(i,2)*cos(robot.angles(i)+previous_angle), corner_3(2) - robot.lengths(i,2)*sin(robot.angles(i)+previous_angle)];
     robot.bodies = [robot.bodies; [corner_1(1) corner_2(1) corner_3(1) corner_4(1)] [corner_1(2) corner_2(2) corner_3(2) corner_4(2)]];
+    robot.maxheight = max(max(robot.bodies(:,5:8)));
 
     if draw =="draw"
         newcolors = [[0 0.4470 0.7410] ; [0.8500 0.3250 0.0980] ; [0.9290 0.6940 0.1250] ; [0.4940 0.1840 0.5560] ; [0.4660 0.6740 0.1880] ; [0.3010 0.7450 0.9330] ; [0.6350 0.0780 0.1840]];
@@ -56,7 +58,7 @@ for i = 1:size(lengths,1)
             [corner_1(2), corner_2(2), corner_3(2), corner_4(2)],c);
         hold on;
     end
-    next_base = (corner_3+corner_2)./2;
+    next_base = (((2*corner_3+corner_4)./3)+(2*corner_2+corner_1)./3)./2;
     previous_angle = robot.angles(i)+previous_angle;
 
 
--- Homeworks/HW3/sources/distanceMetric.m
+++ Homeworks/HW3/sources/distanceMetric.m
@@ -4,10 +4,28 @@ function distance = distanceMetric(nodes1, nodes2)
 %   nodes2 = [z1 z1 alpha1 alpha2; z2 z2 alpha1 alpha2; ...]
 
 % global nodes;
-weight = ones(size(nodes1(1,:)));
+weight = [1 1 1 1];
+nodes1_height = zeros(size(nodes1,1),1);
+nodes2_height = zeros(size(nodes2,1),1);
+for i = 1 : size(nodes1,1)
+    rob = createRobot(nodes1(i,:),"do not plot");
+    nodes1_height(i) = rob.maxheight;
+
+end
+
+for i = 1 : size(nodes2,1)
+    rob = createRobot(nodes2(i,:),"do not plot");
+    nodes2_height(i) = rob.maxheight;
+
+end
+
+height_penalty = (nodes1_height + nodes2_height)'
+
+
 nodes1 = nodes1.*weight;
 nodes2 = nodes2.*weight;
-distance = sqrt((sum((nodes1'-nodes2').^2)));
+distance = sqrt((sum((nodes1'-nodes2').^2)))
+distance = distance + height_penalty-5
 
 % distance = sqrt(sum((nodes(s,:)-nodes(t,:)).^2'))
 
