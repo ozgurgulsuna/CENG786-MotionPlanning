c977f70ad56c29608d1496262761ba1d3d4986cd
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-12-29 23:08:19 +0300

    [HW3] final upload

--- Homeworks/HW3/sources/plotPath.m
+++ Homeworks/HW3/sources/plotPath.m
@@ -20,6 +20,7 @@ for i = 1:length(path)-1
     for i = 1:N
         q = (N-i)/N*current_configuration + i/N*next_configuration;
         createRobot(q,"draw");
+        pause(0.015);
     end
 
 end
